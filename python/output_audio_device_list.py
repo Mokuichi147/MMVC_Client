c7ffe2004853f5ca9d2b96a9faf8de5dca9f3115
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2023-01-13 02:33:48 +0900

    sounddeviceによるasioのサポート

--- python/output_audio_device_list.py
+++ python/output_audio_device_list.py
@@ -1,32 +1,20 @@
-# -*- coding: utf-8 -*
-import pyaudio
-from os import linesep
+import sounddevice as sd
 
 def main():
-    audio = pyaudio.PyAudio()
     audio_devices = list()
-    host_apis = list()
+    host_apis = sd.query_hostapis()
     
-    for api_index in range(audio.get_host_api_count()):
-        host_apis.append(audio.get_host_api_info_by_index(api_index)['name'])
-    
-    # 音声デバイス毎のインデックス番号を一覧表示
-    for x in range(0, audio.get_device_count()):
-        devices = audio.get_device_info_by_index(x)
-        try:
-            device_name = devices['name'].encode('shift-jis').decode('utf-8')
-        except UnicodeDecodeError:
-            device_name = devices['name']
-        
-        device_name = device_name.replace(linesep, '') + ", " + host_apis[devices['hostApi']]
+    for index, device in enumerate(sd.query_devices()):
+        device_name = device['name'] + ", " + host_apis[device['hostapi']]['name']
         
         isInOut = ""
-        if devices['maxInputChannels'] > 0:
+        if device['max_input_channels'] > 0:
             isInOut += "入"
-        if devices['maxOutputChannels'] > 0:
+        
+        if device['max_output_channels'] > 0:
             isInOut += "出"
         
-        audio_devices.append(f"{isInOut}力： Index：{devices['index']} デバイス名：\"{device_name}\"\n")
+        audio_devices.append(f'{isInOut}力： Index：{index} デバイス名："{device_name}"\n')
 
     with open('audio_device_list.txt', 'w', encoding='utf-8') as f:
         f.writelines(audio_devices)
